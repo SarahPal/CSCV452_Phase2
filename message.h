b50141940b1c2778580e3607304a89145652cccd
SarahPal <47961885+SarahPal@users.noreply.github.com>
2019-03-14 13:38:01 -0700

    Update message.h
    Slot size added. slot status still needed?

--- message.h
+++ message.h
@@ -11,8 +11,9 @@ typedef struct mbox_proc *mbox_proc_ptr;
 
 struct mailbox {
    int           mbox_id;
-   int           slot_status;
+   //int           slot_status;
    int           num_slots;
+   int           slot_size;
    /* other items as needed... */
 };
 
