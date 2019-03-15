651368593ce599320d2ab99e1778d7891f408d2d
SarahPal <47961885+SarahPal@users.noreply.github.com>
2019-03-14 21:51:58 -0700

    check_io()
    Added incomplete check_io().

--- p1.c
+++ p1.c
@@ -9,3 +9,10 @@ p1_switch(int old, int new)
 void
 p1_quit(int pid)
 {}
+
+//REDEFINE REVIEW FEB 22 MEETING
+void check_io()
+{
+    //Return 1 if at least one process is blocked on an I/O MailBox
+    //Return 0 otherwise
+}
