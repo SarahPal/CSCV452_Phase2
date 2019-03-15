763d8cc5d69748ce7f8a7d21a8b57f56dc9b8a0d
SarahPal <47961885+SarahPal@users.noreply.github.com>
2019-03-14 21:51:00 -0700

    Fixed Errors & Inserted Comments
    Fixed previous errors. Included comments on functions from Phase 2 Project Notes. Added TODO statements.

--- phase2.c
+++ phase2.c
@@ -9,6 +9,7 @@
 #include <phase1.h>
 #include <phase2.h>
 #include <usloss.h>
+#include <stdio.h>
 
 #include "message.h"
 
@@ -32,6 +33,8 @@ int numMailboxes = 0;
 
 /* the mail boxes */
 mail_box MailBoxTable[MAXMBOX];
+//slot_ptr Mail_Slots[] Not sure what to do here, but I know it's needed...
+//TODO: Define a new Process Table
 
 
 /* -------------------------- Functions ----------------------------------- */
@@ -46,6 +49,9 @@ mail_box MailBoxTable[MAXMBOX];
    ----------------------------------------------------------------------- */
 int start1(char *arg)
 {
+
+    int kid_pid;
+    int status;
    if (DEBUG2 && debugflag2)
       console("start1(): at beginning\n");
 
@@ -59,22 +65,24 @@ int start1(char *arg)
     * handlers.  Etc... */
    for(int i = 0; i <= MAXMBOX; i++)
    {
-      MailBoxTable[i]->mbox_id = i;
-      MailBoxTable[i]->slot_status = UNUSED;
+      MailBoxTable[i].mbox_id = i;
+      MailBoxTable[i].status = INACTIVE;
    }
-    
-   //initialize int_vec
-   //initialize sys_vec
-   
-   IntHandMB[CLOCKMB] = MBoxCreate(0, sizeof(int)); //Clock MB
-   IntHandMB[TERMMB] = MBoxCreate(0, sizeof(int)); //Terminal 1
-   IntHandMB[TERMMB + 1] = MBoxCreate(0, sizeof(int)); //Terminal 2
-   IntHandMB[TERMMB + 2] = MBoxCreate(0, sizeof(int)); //Terminal 3
-   IntHandMB[TERMMB + 3] = MBoxCreate(0, sizeof(int)); //Terminal 4
-   IntHandMB[DISKMB] = MBoxCreate(0, sizeof(int)); //Disk 1
-   IntHandMB[DISKMB + 1] = MBoxCreate(0, sizeof(int)); //Disk 2
-   
-   
+   //TODO: Initialze mail_slots table
+   //TODO: Initialize new process table
+   //TODO: initialize int_vec
+   //TODO: initialize sys_vec
+   int IntHandMB[7];
+
+   IntHandMB[CLOCKMB] = MboxCreate(0, sizeof(int)); //Clock MB
+   IntHandMB[TERMMB] = MboxCreate(0, sizeof(int)); //Terminal 1
+   IntHandMB[TERMMB + 1] = MboxCreate(0, sizeof(int)); //Terminal 2
+   IntHandMB[TERMMB + 2] = MboxCreate(0, sizeof(int)); //Terminal 3
+   IntHandMB[TERMMB + 3] = MboxCreate(0, sizeof(int)); //Terminal 4
+   IntHandMB[DISKMB] = MboxCreate(0, sizeof(int)); //Disk 1
+   IntHandMB[DISKMB + 1] = MboxCreate(0, sizeof(int)); //Disk 2
+
+
    enableInterrupts();
 
    /* Create a process for start2, then block on a join until start2 quits */
@@ -91,41 +99,41 @@ int start1(char *arg)
 
 /* ------------------------------------------------------------------------
    Name - MboxCreate
-   Purpose - gets a free mailbox from the table of mailboxes and initializes it 
+   Purpose - gets a free mailbox from the table of mailboxes and initializes it
    Parameters - maximum number of slots in the mailbox and the max size of a msg
                 sent to the mailbox.
    Returns - -1 to indicate that no mailbox was created, or a value >= 0 as the
              mailbox id.
-   Side Effects - initializes one element of the mail box array. 
+   Side Effects - initializes one element of the mail box array.
    ----------------------------------------------------------------------- */
 int MboxCreate(int slots, int slot_size)
 {
-   disableinterrupts();
+   disableInterrupts();
    check_kernel_mode("MboxCreate");
-   int next_Available = getinactive(); 
-   
+   int next_Available = getinactive();
+
    if(next_Available == -1)
    {
       if(DEBUG2 && debugflag2)
       {
-         console("MboxCreate: max boxes reached, returning -1");
+         console("MboxCreate: max boxes reached, returning...");
       }
       return -1;
    }
-   mailbox *nBox = &MailBoxTable[next_Available];
-    
-   nBox->mboxID = next_Available++;
+   mail_box *nBox = &MailBoxTable[next_Available];
+
+   nBox->mbox_id = next_Available++;
    nBox->num_slots = slots;
    nBox->slot_size = slot_size;
-   nBox->status = ACTIVE;     
-   
+   nBox->status = ACTIVE;
+
    if(DEBUG2 && debugflag2)
    {
-      console("Created mailbox with id %d, total slots = %d, slot size = %d\n", nBox->mBoxID, nBox->num_slots,
-              nBox->slot_size);
-   }      
-   enableinterrupts();
-   return nBox->mboxID;
+      console("Created mailbox with id %d, total slots = %d, slot size = %d\n",
+      nBox->mbox_id, nBox->num_slots,nBox->slot_size);
+   }
+   enableInterrupts();
+   return nBox->mbox_id;
 } /* MboxCreate */
 
 
@@ -139,6 +147,37 @@ int MboxCreate(int slots, int slot_size)
    ----------------------------------------------------------------------- */
 int MboxSend(int mbox_id, void *msg_ptr, int msg_size)
 {
+    disableInterrupts();
+    int mbStatus = MailBoxTable[mbox_id].status;
+    if(DEBUG2 && debugflag2)
+        console("MboxSend(): Checking for possible errors...");
+    //Check for possible errors
+    if(mbStatus == INACTIVE)
+    {
+        //Some error message
+        return -1;
+    }
+    if(msg_size > max_message_size)
+    {
+        //some error message
+        return -1;
+    }
+
+    //If slot is available in this mailbox, allocate a slot from the mail_slot
+    //table
+    //TODO: This crap
+    //If the Mail slot overflows, halt usloss
+    //TODO: This crap
+
+    //Copy message into allocated slot (use memcpy or strcpy)
+    //TODO: This crap
+
+    //Block sender if mailbox has no available slots.
+    //TODO: This crap
+
+    enableInterrupts();
+    return 0;
+
 } /* MboxSend */
 
 
@@ -153,14 +192,35 @@ int MboxSend(int mbox_id, void *msg_ptr, int msg_size)
    ----------------------------------------------------------------------- */
 int MboxReceive(int mbox_id, void *msg_ptr, int msg_size)
 {
+    disableInterrupts();
+    int mbStatus = MailBoxTable[mbox_id].status;
+
+    if(mbStatus == INACTIVE)
+    {
+        //Some error message
+        return -1;
+    }
+
+    //If one (or more) messages are available in the mailbox, memcpy the
+    //message from the slot to the receiver's buffer
+    //TODO: This crap
+
+    //Free the mailbox slot
+    //TODO: This crap
+
+    //Block the receiver if there are not messages in the mailbox
+    //TODO: This crap
+
+    enableInterrupts();
+    return msg_size;
 } /* MboxReceive */
 
 
 /* ------------------------------------------------------------------------
    Name - MboxRelease
-   Purpose - Releases a previously created mailbox. 
+   Purpose - Releases a previously created mailbox.
    Parameters - mailbox id
-   Returns - 
+   Returns -
          -3: process was zap'd while releasing the mailbox.
          -1: the mailboxID is not a mailbox that is in use.
           0: successful completion.
@@ -178,7 +238,7 @@ int MboxRelease(int mailboxID)
              Does not block the invoking process.
    Parameters - mailbox id, pointer to put data of msg, max # of bytes that
                 can be received.
-   Returns - 
+   Returns -
          -3: process is zap'd.
          -2: mailbox full, message not sent; or no mailbox slots available in the system.
          -1: illegal values given as arguments.
@@ -192,15 +252,15 @@ int MboxCondSend(int mailboxID, void *message, int message_size)
 
 /* ------------------------------------------------------------------------
    Name - MboxCondReceive
-   Purpose - Conditionally receive a message from the mailbox. 
+   Purpose - Conditionally receive a message from the mailbox.
              Does not block the invoking process.
    Parameters - mailbox id, pointer to put data of msg, max # of bytes that
                 can be received.
-   Returns - 
+   Returns -
           -3: process is zap'd.
           -2: mailbox empty, no message to receive.
           -1: illegal values given as arguments; or, message sent is too large
-              for receiver's buffer (no data copied in this case). 
+              for receiver's buffer (no data copied in this case).
           The size of the message received.
    Side Effects - none.
    ----------------------------------------------------------------------- */
@@ -213,7 +273,7 @@ int MboxCondReceive(int mailboxID, void *message, int max_message_size)
    Name - waitdevice
    Purpose - Synchronize processes with interrupt handlers.
    Parameters - the type of device, the unit, and the current status.
-   Returns - 
+   Returns -
            -1: the process was zapped while waiting
             0: otherwise
    Side Effects - The appropriate device mailbox is sent a message every
@@ -221,6 +281,10 @@ int MboxCondReceive(int mailboxID, void *message, int max_message_size)
    ----------------------------------------------------------------------- */
 int waitdevice(int type, int unit, int *status)
 {
+    //Called when a process wants to receive results of i/o operations
+    //Applicable to Test 13 and test14
+
+    //use MboxReceive on the appropriate mailbox.
 } /* waitdevice */
 
 
@@ -236,5 +300,3 @@ int getInactive()
    }
    return -1;
 }
-
-
